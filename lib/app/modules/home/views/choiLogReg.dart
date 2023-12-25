7c39e466c0a8c4682aeec2a759a07ac5961d6e3d
318-Annisa <a.walidatus@gmail.com>
2023-12-25 10:46:07 +0700

    desain

--- lib/app/modules/home/views/choiLogReg.dart
+++ lib/app/modules/home/views/choiLogReg.dart
@@ -9,26 +9,43 @@ class Choise extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Get.to(() => Login());
-                print('Login button pressed');
-              },
-              child: Text('Login'),
-            ),
-            SizedBox(height: 20), // Spasi antara tombol
-            TextButton(
-              onPressed: () {
-                Get.to(() => Register());
-                print('Navigate to Register Page');
-              },
-              child: Text('Register'),
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Get.to(() => Login());
+                  print('Login button pressed');
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 171, 83, 11),
+                ),
+                child: Text(
+                  'Login',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              SizedBox(height: 20), // Spasi antara tombol
+              TextButton(
+                onPressed: () {
+                  Get.to(() => Register());
+                  print('Navigate to Register Page');
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/app/modules/home/views/login_page.dart
+++ lib/app/modules/home/views/login_page.dart
@@ -1,10 +1,9 @@
 import 'package:flappy_bird/app/modules/home/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class Login extends StatefulWidget {
-  const Login({super.key});
+  const Login({Key? key});
 
   @override
   State<Login> createState() => _LoginState();
@@ -14,6 +13,7 @@ class _LoginState extends State<Login> {
   final AuthController _authController = Get.put(AuthController());
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
   @override
   void dispose() {
     _emailController.dispose();
@@ -25,46 +25,63 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [],
         title: Text('Login'),
+        backgroundColor: Color.fromARGB(255, 87, 187, 209),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _emailController,
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                controller: _emailController,
+                style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                decoration: InputDecoration(
                   labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.grey)),
-            ),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              style: TextStyle(color: Colors.black),
-              decoration: InputDecoration(
+                  labelStyle:
+                      TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                style: TextStyle(color: Colors.black),
+                decoration: InputDecoration(
                   labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.grey)),
-            ),
-            SizedBox(height: 16),
-            Obx(() {
-              return ElevatedButton(
-                onPressed: _authController.isLoading.value
-                    ? null
-                    : () {
-                        _authController.loginUser(
-                          _emailController.text,
-                          _passwordController.text,
-                        );
-                      },
-                child: _authController.isLoading.value
-                    ? CircularProgressIndicator()
-                    : Text('Login'),
-              );
-            }),
-          ],
+                  labelStyle:
+                      TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+              SizedBox(height: 16),
+              Obx(() {
+                return ElevatedButton(
+                  onPressed: _authController.isLoading.value
+                      ? null
+                      : () {
+                          _authController.loginUser(
+                            _emailController.text,
+                            _passwordController.text,
+                          );
+                        },
+                  style: ElevatedButton.styleFrom(
+                    primary: Color.fromARGB(255, 171, 83, 11),
+                  ),
+                  child: _authController.isLoading.value
+                      ? CircularProgressIndicator()
+                      : Text('Login', style: TextStyle(color: Colors.black)),
+                );
+              }),
+            ],
+          ),
         ),
       ),
     );
